3a82b841159e9649af0cb5e7336fd70a0aedcfdc
Wayne A. Tonnesen <wayne.tonnesen@gmail.com>
2018-11-13 19:56:54 -0500

    Added basic requestor class & tests

---
+++ potdora-recipe-requestor/src/test/java/com/potdora/reciperequestor/TestRequestor.java
@@ -0,0 +1,30 @@
+package com.potdora.reciperequestor;
+
+import org.junit.*;
+
+public class TestRequestor {
+
+    int responseCode = 0;
+
+    @Before
+    public void getStatus() {
+
+        try {
+            responseCode = Requestor.requestRecipes();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
+
+    @Test
+    public void testRequestorSuccess() {
+
+        Assert.assertEquals(200, responseCode);
+    }
+
+    @Test
+    public void testRequestorFailure() {
+        Assert.assertNotEquals(100, responseCode);
+    }
+
+}
